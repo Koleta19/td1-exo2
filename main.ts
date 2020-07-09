e40151e4af4256c89f30be6354ca3bc40a762c52
Koleta19 <67999358+Koleta19@users.noreply.github.com>
2020-07-09 16:26:13 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("Hello!")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+}
 basic.forever(function () {
 	
 })
